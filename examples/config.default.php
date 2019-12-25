7555849de5523d2b0a8de900a65140cbebdd612f
Kaique Garcia <kaiquegarciam@gmail.com>
2019-12-25 11:19:51 -0300

    Option to define Player Hash to make Media's embed url.

--- examples/config.default.php
+++ examples/config.default.php
@@ -3,3 +3,7 @@
 require_once realpath(implode(DIRECTORY_SEPARATOR, [__DIR__, "..", "src", "spl_autoload.php"]));
 
 \Sambavideo\API\Settings::setToken("YOUR-TOKEN");
+
+// put project's data from the output of project.php
+$projectId = 0;
+\Sambavideo\API\Settings::setPlayerHash("PLAYER HASH");
